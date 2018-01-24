b258a3cd540653c208d18bf4bf83f735cf68c7f9
Peter Simons <simons@cryp.to>
2018-01-24 20:31:55 +0100

    archive cabal files that generate the golden specs

--- test/Main.hs
+++ test/Main.hs
@@ -21,7 +21,7 @@ regressionTest :: String -> TestTree
 regressionTest goldenFile = do
   let pid = takeBaseName (dropExtension goldenFile)
       PackageIdentifier pn _ = fromMaybe (error ("invalid package id " ++ show pid)) (simpleParse pid)
-      cabalFile = "test" </> "golden-test-cases" </> pid </> unPackageName pn <.> "cabal"
+      cabalFile = "test" </> "golden-test-cases" </> unPackageName pn <.> "cabal"
       specFile = cabalFile -<.> ".spec"
   goldenVsFile pid goldenFile specFile
                (cabal2spec buildPlatform buildCompilerId [] True cabalFile specFile)
